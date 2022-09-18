da47add9735a5b2e6d137ca2bbe97125d6cec681
rkteddy <838474413@qq.com>
2022-09-18 02:36:09 -0700

    added comments

--- defense.py
+++ defense.py
@@ -1,6 +1,14 @@
 import torch
 import torch.nn as nn
 
+"""
+    Input:
+        -net: model to be pruned
+        -u: coefficient that determines the pruning threshold
+    Output:
+        None (in-place modification on the model)
+"""
+
 def CLP(net, u):
     params = net.state_dict()
     for name, m in net.named_modules():
@@ -10,18 +18,18 @@ def CLP(net, u):
 
             channel_lips = []
             for idx in range(weight.shape[0]):
+                # Combining weights of convolutions and BN
                 w = conv.weight[idx].reshape(conv.weight.shape[1], -1) * (weight[idx]/std[idx]).abs()
                 channel_lips.append(torch.svd(w.cpu())[1].max())
             channel_lips = torch.Tensor(channel_lips)
 
             index = torch.where(channel_lips>channel_lips.mean() + u*channel_lips.std())[0]
 
-
             params[name+'.weight'][index] = 0
             params[name+'.bias'][index] = 0
             print(index)
         
-    #     # Convolutional layer should be followed by a BN layer by default
+       # Convolutional layer should be followed by a BN layer by default
         elif isinstance(m, nn.Conv2d):
             conv = m
 
